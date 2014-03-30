51c02a2451cec4ec77f82bc22a0cb209207a16df
rob <robert.schultheis@gmail.com>
2014-03-30 16:32:48 -0400

    cleaner debug output of configuration

--- spec/support/configuration.rb
+++ spec/support/configuration.rb
@@ -4,6 +4,6 @@ RSpec.configure do |config|
   config.include TCFG::Helper
 
   config.before(:all) do
-    debug "Executing tests with this configuration:\n#{tcfg.to_yaml}"
+    debug "Executing tests with this configuration:\n#{tcfg.to_hash.to_yaml}"
   end
 end
