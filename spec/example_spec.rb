f3f32e3cdf7d9a8e68e0db0892c83626cdeec0dd
robert schultheis <robertschultheis@roberts-MBP.lan>
2023-02-15 16:01:28 -0700

    update example spec logic

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -5,8 +5,9 @@ describe 'searching Google.com' do
 
   # this test should pass
   it "should allow me to search for 'green cheese'", tag('Smoke') do
-    fill_in('q', with: "green cheese\n")
-    expect(page).to have_content('unripened or unmatured cheese.')
+    fill_in('q', with: "green cheese")
+    send_keys Selenium::WebDriver::Keys[:return]
+    expect(page).to have_content('Green cheese is a fresh cheese')
   end
 
   # this test should fail
